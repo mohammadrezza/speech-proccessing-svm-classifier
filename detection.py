e7f410883496217c5e475a62b8f39b75c3a0f19d
Mohammadreza Mirhajianmoghadam <mrmirhajian1996@gmail.com>
2018-12-24 21:09:41 +0330

    pca splitted into two functions, added predicting with svm(SVC) classifier

--- detection.py
+++ detection.py
@@ -1,24 +1,19 @@
 from sklearn.svm import SVC
 from sklearn.decomposition import PCA
+import scipy.io.wavfile as wav
 import os
 import numpy as np
-from utility import load
+from utility import load, play
 from blueprints import *
+from feature_extraction import extract
 
 
-def perform_pca(x, n):
-    """
-    if there are too many features versus few dataset, PCA is essential
-    to achive better result.
-    :param x: array-like, consist the features
-    :param n: int, drop features into N features, it should be bigger than the size of the X
-    :return: array-like, consist the features dropped to N
-    """
-    pca = PCA(n_components=n, whiten=True)
-    pca.fit(np.array(x))
-    pca_x = pca.transform(np.array(x))
+def pca_fit(x):
+    pca_obj.fit(np.array(x))
+
+def pca_transform(x):
+    pca_x = pca_obj.transform(np.array(x))
     return pca_x
-
 
 def classify(x, y):
     clf = SVC(C=1, gamma='auto', probability=True)
@@ -26,11 +21,38 @@ def classify(x, y):
     return clf
 
 
+def keep_predicting():
+    # remove previous files
+    for file in os.listdir(TEST_PATH):
+        os.remove(os.path.join(TEST_PATH, file))
+    while True:
+        try:
+            for file in os.listdir(TEST_PATH):
+                rate, sig = wav.read(os.path.join(TEST_PATH, file))
+                feat = extract(sig)
+                pca_feats = pca_transform([feat])
+                result = words_clf.predict(pca_feats)
+                result2 = gender_clf.predict(pca_feats)
+                play(result[0])
+                play(result2[0])
+                os.remove(os.path.join(TEST_PATH, file))
+        except Exception as e:
+            print(e.__str__())
+
+
 if __name__ == "__main__":
+    # reading the data from saved models in train
     features = load(os.path.join(MODELS_PATH, WORDS_FEATURES))
     words_labels = load(os.path.join(MODELS_PATH, WORDS_LABLES))
     gender_labels = load(os.path.join(MODELS_PATH, GENDER_LABLES))
 
-    pca_feats = perform_pca(features, 50)
+    pca_obj = PCA(n_components=50, whiten=True)
+    pca_fit(features)
+    pca_feats = pca_transform(features)
     words_clf = classify(pca_feats, words_labels)
     gender_clf = classify(pca_feats, gender_labels)
+
+    print("words score :", words_clf.score(pca_feats, words_labels) * 100)
+    print("gender score :", gender_clf.score(pca_feats, gender_labels) * 100)
+
+    keep_predicting()
